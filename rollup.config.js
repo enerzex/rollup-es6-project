c8fff51dcb0c08d6801a7203c9d5905e77d06547
Jinpyo, Kim <jpspace@nate.com>
2018-08-15 18:01:31 +0900

    eslint 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,18 @@
 import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
 
 export default {
   input: 'public/js/index.js',
   output: {
     file: 'public/js/bundle.js',
-    format: 'iife'
+    format: 'iife',
   },
   plugins: [
     babel({
 	    exclude: 'node_modules/**',
-    })
+    }),
+    eslint({
+      include: './public/js/**',
+    }),
   ],
 };
